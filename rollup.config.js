c81140730363c90b5c32e19f3d75899bc6b05f70
Michał Żarłok <michal.zarlok@pega.com>
2019-12-23 13:29:39 +0100

    codestyle cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -5,33 +5,33 @@ import json from 'rollup-plugin-json';
 
 
 const commonPlugins = [
-    json({
-        include: [
-            'node_modules/moment-timezone/**'
-        ],
-        compact: true,
-        preferConst: true
-    }),
-    commonjs({
-        include: [
-            'node_modules/moment/**',
-            'node_modules/moment-timezone/**',
-            'node_modules/humanize-duration-ts/**'
-        ],
-        sourceMap: false
-    }),
-    resolve({
-        mainFields: ['main']
-    }),
-    typescript()
+  json({
+    include: [
+      'node_modules/moment-timezone/**',
+    ],
+    compact: true,
+    preferConst: true,
+  }),
+  commonjs({
+    include: [
+      'node_modules/moment/**',
+      'node_modules/moment-timezone/**',
+      'node_modules/humanize-duration-ts/**',
+    ],
+    sourceMap: false,
+  }),
+  resolve({
+    mainFields: ['main'],
+  }),
+  typescript(),
 ];
 
 export default {
-    input: 'src/card.ts',
-    output: {
-        file: 'sun-card.js',
-        format: 'esm',
-        compact: true
-    },
-    plugins: [...commonPlugins]
-}
+  input: 'src/card.ts',
+  output: {
+    file: 'sun-card.js',
+    format: 'esm',
+    compact: true,
+  },
+  plugins: [...commonPlugins],
+};

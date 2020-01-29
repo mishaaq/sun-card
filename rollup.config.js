7b89c5e88a0efd5e5c2164c62030e6ed4aa2b392
Michał Żarłok <michal.zarlok@pega.com>
2020-01-29 10:41:41 +0100

    support hot-reload for dev env, codestyle config update

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+
+const dev = process.env.ROLLUP_WATCH;
 
 const commonPlugins = [
   resolve(),
@@ -21,13 +24,22 @@ const commonPlugins = [
     compact: true,
     preferConst: true,
   }),
-  terser(),
+  dev && serve({
+    contentBase: 'dist',
+    host: '0.0.0.0',
+    port: 5000,
+    allowCrossOrigin: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  }),
+  !dev && terser(),
 ];
 
 export default {
   input: 'src/card.ts',
   output: {
-    file: 'sun-card.js',
+    file: 'dist/sun-card.js',
     format: 'es',
   },
   plugins: [...commonPlugins],
